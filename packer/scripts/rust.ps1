dfe4603d5203faac142f7a729bb96f73bc16523e
oott123 <git@public.oott123.com>
2023-07-12 23:47:52 +0800

    chore: just don't install rustup

--- packer/scripts/rust.ps1
+++ packer/scripts/rust.ps1
@@ -1,5 +1,5 @@
 . C:\image\guard.ps1
 
-Exec-CommandRetry -Delay 5000 -Maximum 20 { choco install rustup.install --version 1.25.1 -y }
+# Exec-CommandRetry -Delay 5000 -Maximum 20 { choco install rustup.install --version 1.25.1 -y }
 Exec-CommandRetry -Delay 5000 -Maximum 20 { choco install llvm --version 16.0.6 -y }
 Exec-CommandRetry -Delay 5000 -Maximum 20 { choco install cmake.install --version 3.26.4 -y }
